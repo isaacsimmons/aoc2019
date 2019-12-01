25f89202e8fe6eb7d95b83cfd7a248597f9bf958
Isaac Simmons <ids23@drexel.edu>
2019-12-01 11:34:49 -0500

    Day1Part2

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -3,7 +3,13 @@ import { readFileSync } from 'fs';
 const input = readFileSync('src/day1/input.txt').toString();
 const numbers = input.split('\n').filter(x => x.length > 0).map(Number);
 
-const fuelRequired = (mass: number) => Math.floor(mass / 3) - 2;
+const fuelRequired = (mass: number): number => {
+    const fuel = Math.floor(mass / 3) - 2;
+    if (fuel <= 0) {
+        return 0;
+    }
+    return fuel + fuelRequired(fuel);
+}
 
 let totalFuel = numbers.map(fuelRequired).reduce((x, y) => x + y, 0);
 console.log(totalFuel);
