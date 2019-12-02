7716339ba8d4df99414557b24b624784ab09aaed
Isaac Simmons <ids23@drexel.edu>
2019-12-02 07:33:13 -0500

    Rename vars to match problem vocab

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -3,85 +3,85 @@ import { readFileSync } from 'fs';
 const FILENAME = 'src/day2/input.txt';
 
 const inputText = readFileSync(FILENAME).toString().trim();
-const program = inputText.split(',').map(Number);
+const memory = inputText.split(',').map(Number);
 
-program[1] = 12;
-program[2] = 2;
+memory[1] = 12;
+memory[2] = 2;
 
 type AccessType = 'operator' | 'read' | 'write';
-enum Operator {
+enum OpCode {
     addition = 1,
     multiplication = 2,
     terminate = 99,
 }
 
-let state: ProgramState = { position: 0, terminated: false };
+let state: ProgramState = { address: 0, terminated: false };
 
-const checkBounds = (idx: number, accessType: AccessType) => {
-    if (idx < 0 || idx >= program.length) {
-        throw new Error(`Attempted ${accessType} access at position ${idx} (program at ${state.position})`);
+const checkBounds = (address: number, accessType: AccessType) => {
+    if (address < 0 || address >= memory.length) {
+        throw new Error(`Attempted ${accessType} access at position ${address} (program at ${state.address})`);
     }
 }
 
-function assertIsOperator(n: number): asserts n is Operator {
-    if (!Object.values(Operator).includes(n)) {
+function assertIsOperator(n: number): asserts n is OpCode {
+    if (!Object.values(OpCode).includes(n)) {
         throw new Error(`Unknown command: ${n}`);
     }
 };
 
 interface ProgramState {
-    position: number;
+    address: number;
     terminated: boolean;
 }
 
-const doAddition = (position: number) => {
-    arity2(position, (x, y) => x + y);
+const doAddition = (address: number) => {
+    arity2(address, (x, y) => x + y);
 }
 
-const doMultiplication = (position: number) => {
-    arity2(position, (x, y) => x * y);
+const doMultiplication = (address: number) => {
+    arity2(address, (x, y) => x * y);
 }
 
-const arity2 = (position: number, operator: (n1: number, n2: number) => number) => {
-    checkBounds(position + 1, 'operator');
-    checkBounds(position + 2, 'operator');
-    checkBounds(position + 3, 'operator');
-    const idx1 = program[position + 1];
-    const idx2 = program[position + 2];
-    const idxTarget = program[position + 3];
+const arity2 = (address: number, operator: (n1: number, n2: number) => number) => {
+    checkBounds(address + 1, 'operator');
+    checkBounds(address + 2, 'operator');
+    checkBounds(address + 3, 'operator');
+    const idx1 = memory[address + 1];
+    const idx2 = memory[address + 2];
+    const idxTarget = memory[address + 3];
 
     checkBounds(idx1, 'read');
     checkBounds(idx2, 'read');
-    const n1 = program[idx1];
-    const n2 = program[idx2];
+    const n1 = memory[idx1];
+    const n2 = memory[idx2];
     const result = operator(n1, n2);
     checkBounds(idxTarget, 'write');
-    program[idxTarget] = result;
+    memory[idxTarget] = result;
 }
 
 const runStep = (initial: ProgramState): ProgramState => {
-    const { position, terminated } = initial;
+    const { address, terminated } = initial;
     if (terminated) {
         throw new Error('Trying to run an already terminated program');
     }
-    checkBounds(position, 'operator');
-    const operator = program[initial.position];
+    checkBounds(address, 'operator');
+    const operator = memory[initial.address];
     assertIsOperator(operator);
     switch (operator) {
-        case Operator.addition:
-            doAddition(position);
+        case OpCode.addition:
+            doAddition(address);
             break;
-        case Operator.multiplication:
-            doMultiplication(position);
+        case OpCode.multiplication:
+            doMultiplication(address);
             break;
-        case Operator.terminate:
+        case OpCode.terminate:
             return { ...initial, terminated: true };
     }
-    return { position: position + 4, terminated: false };
+    return { address: address + 4, terminated: false };
 } 
 
-console.log('Input', program);
+console.log('Input', memory);
 while (!state.terminated) {
     state = runStep(state);
 }
-console.log('Final state', program);
+console.log('Final state', memory);
