e5f80833ccb0d0cadf427cf2302d60612a5e2fc9
Isaac Simmons <ids23@drexel.edu>
2019-12-04 06:52:32 -0500

    Brain-dead brute force solution

--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -2,19 +2,24 @@ const MIN = 273025;
 const MAX = 767253;
 
 const testNumber = (n: Number): boolean => {
-    const digits = String(n);
+    const digits = String(n).split('').map(Number);
     let hasDouble = false;
+    const buckets = [0,0,0,0,0,0,0,0,0,0];
     for (let i = 0; i < 5; i++) {
-        if (digits[i] === digits[i+1]) {
-            hasDouble = true;
-        }
         if (digits[i] > digits[i+1]) {
             return false;
         }
     }
-    return hasDouble;
+    for (let i = 0; i <= 5; i++) {
+        buckets[digits[i]]++;
+    }
+    return buckets.filter(n => n === 2).length > 0;
 }
 
+
+console.log(testNumber(112233));
+console.log(testNumber(123444));
+console.log(testNumber(111122));
 
 let n = 0;
 for (let i = MIN; i <= MAX; i++) {
