9941a3522e7fb6553abe4908a53c7a65f0da82c5
Isaac Simmons <ids23@drexel.edu>
2019-12-08 10:20:11 -0500

    Colorize output

--- src/image/Layer.ts
+++ src/image/Layer.ts
@@ -1,6 +1,17 @@
 import Dimensions from "./Dimensions";
 import { chunk } from "../utils/array";
 
+const COLOR_RESET = '\x1b[0m';
+
+// More colors here: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+const COLOR_ESCAPE: {[key: string]: string} = {
+    '0': '\x1b[40;37m', // BLACK: black bg, white fg
+    '1': '\x1b[107;90m', // WHITE: bright white bg, bright black fg
+    '2': '\x1b[97;101m', // TRANSPARENT: red bg, bright white fg
+};
+
+const colorize = (pixel: string) => (COLOR_ESCAPE[pixel] || COLOR_RESET) + pixel;
+
 export default class Layer {
     static readonly TRANSPARENT = '2';
 
@@ -9,7 +20,7 @@ export default class Layer {
     print() {
         const rows = chunk(this.pixels, this.dimensions.x);
         for (const row of rows) {
-            console.log(row.map(digit => digit === '1' ? 'X' : ' ').join(''));
+            console.log(row.map(colorize).join('') + COLOR_RESET);
         }
     }
 
