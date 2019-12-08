6c2890bfed258ab615c1974dc3f7993eb247bd03
Isaac Simmons <ids23@drexel.edu>
2019-12-08 11:25:29 -0500

    Consolidate 'digits' and 'buckets', clean out a bunch of unused code

--- src/day4/main.ts
+++ src/day4/main.ts
@@ -1,128 +1,68 @@
-import { equal } from "assert";
 import { equals } from "../utils/array";
 
 const MIN = 273025;
 const MAX = 767253;
-//const MAX = 289998;
 
 // The "0" element is technically wasted since "ascending" numbers will never have zeroes in them
 // TODO: keep track of max and min digit as well as count of digits
 type Buckets = [number, number, number, number, number, number, number, number, number, number];
-type Digits = number[];
 
 const EMPTY_BUCKETS: Buckets = [0,0,0,0,0,0,0,0,0,0];
 
-// Number to digits
-const ntod = (n: number): Digits => String(n).split('').map(Number);
-
-// Digits to number
-// TODO: write this if I ever need it
-const dton = (d: Digits): number => 42;
-
-// Buckets to digits
-const btod = (b: Buckets): Digits => {
-    const d: number[] = [];
-    b.forEach((count, idx) => {
-        d.push(...Array.from({length: count}).map(_ => idx));
-    });
-    return d;
-}
-
-// Digits to buckets
-const dtob = (d: Digits): Buckets => {
+const bukkit = (n: number): Buckets => {
     const b = [...EMPTY_BUCKETS] as Buckets;
-    d.forEach(digit => b[digit]++);
-    return b;
-};
 
-const hasDouble = (b: Buckets) => b.find(count => count === 2) !== undefined;
+    // Split the number into digits
+    const d = String(n).split('').map(Number);
 
-// Take a potentially non-ascending set of digits and increments it to the next "ascending" value
-const makeAscending = (d: Digits) => {
+    // Round up to the nearest ascending value while bucketing digits
     let min = 0;
     d.forEach((n, idx) => {
         if (n < min) {
             d[idx] = min;
+            b[min]++;
         } else {
             min = n;
+            b[n]++;
         }
     });
+    return b;
 }
 
 // Increment an already ascending set of digits to the next highest value
-const incD = (d: Digits, max: Digits): boolean => {
-    // Scan the number from least significant to most significant looking for a number that we can increment
-    let i = d.length - 1;
-    let changedValue = d[i];
-    while (i > 0 && changedValue === 9) {
-        i--;
-        changedValue = d[i];
-    }
-
-    // Perform the increment (to the selected digit and every one to the right)
-    let equalsMax = true;
-    changedValue++;
-    for (let j = i; j < d.length; j++) {
-        d[j] = changedValue;
-        if (changedValue !== max[j]) {
-            equalsMax = false;
-        }
-    }
-
-    // If any of the digits we already looked at differ from the max
-    if (!equalsMax) {
-        return false;
-    }
-    // ... or any of the digits we didnt' modify differ from the max
-    for (let j = i - 1; j >= 0; j--) {
-        if (d[j] !== max[j]) {
-            // Then the new value isn't the max
-            return false;
-        }
-    }
-    // Otherwise it must now equal the max value (awful lot of extra logic to avoid comparing two tiny arrays)
-    return true;
-}
-
-// Increment an already ascending set of digits to the next highest value
-const incB = (d: Buckets, max: Buckets): boolean => {
+const incB = (b: Buckets, max: Buckets): boolean => {
     // Scan from the highest value looking for one that you can increment
     // Scan the number from least significant to most significant looking for a number that we can increment
 
     // Find the first non-9 value and we can increment that
     for (let i = 8; i >= 1; i--) {
-        if (d[i] > 0) {
-            d[i]--;
-            d[i+1]++;
+        if (b[i] > 0) {
+            b[i]--;
+            b[i+1]++;
             if (i !== 8) {
-                d[i+1] += d[9];
-                d[9] = 0;
+                b[i+1] += b[9];
+                b[9] = 0;
             }
-            return equals(d, max);
+            return equals(b, max);
         }
     }
 
-    // Wrap around to adding another digit, start at all 1's
-    d[1] = d[9] + 1;
-    d[9] = 0;
-    return equals(d, max);
+    // Previous number was all 9'a. Adding another digit, start at all 1's
+    b[1] = b[9] + 1;
+    b[9] = 0;
+    return equals(b, max);
 }
 
 const hasPair = (b: Buckets) => b.some(d => d === 2);
 
-const dx = ntod(MIN);
-makeAscending(dx);
-const bx = dtob(dx);
-
-const dmax = ntod(MAX + 1); // We're using a non-inclusive MAX, but the inputs specify inclusive, so add one to it
-makeAscending(dmax); // TODO: should this be baked into ntod()?
-const bmax = dtob(dmax);
+const x = bukkit(MIN);
+const max = bukkit(MAX + 1); // We're using a non-inclusive MAX, so add 1
 
 let numPasswords = 0;
 do {
-    if (hasPair(bx)) {
+    if (hasPair(x)) {
         numPasswords++;
     }
-} while (!incB(bx, bmax));
+} while (!incB(x, max));
 
 console.log(numPasswords);
