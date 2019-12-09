eaf327398a41339b542ff0f7fad56090290ceb01
Isaac Simmons <isaac.simmons@gmail.com>
2019-12-09 07:39:24 -0500

    "Part 2"

--- src/day9/main.ts
+++ src/day9/main.ts
@@ -4,12 +4,14 @@ import Computer from '../compute/Computer';
 const inputText = readInputFile(Number(process.env.DAY), process.env.FILE);
 const program = inputText.trim().split(',').map(Number);
 
-const computer = new Computer(program, [1]);
+const computer = new Computer(program, [2]);
 
 // "Test 0"
 // const computer = new Computer([109,19,204,-34, 99], []);
 // computer.memory.relativeBase = 2000;
 // computer.memory.write({num: 1985, mode: "position"}, 444);
+
+// TODO: proper bounds check on large numbers
 
 const run = async () => {
     computer.run();
