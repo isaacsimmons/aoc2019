3cb6616dfef67962506cf758bb0e29fc4c5dc66d
Isaac Simmons <isaac.simmons@gmail.com>
2019-12-13 18:54:54 -0500

    grab some max dimensions

--- src/day13/main.ts
+++ src/day13/main.ts
@@ -24,17 +24,28 @@ const runProgram = async () => {
 };
 
 const go = async () => {
+    let maxX = 0;
+    let maxY = 0;
     const outputs = await runProgram();
     const screen = new Map<string, number>();
     for (let i = 0; i < outputs.length; i += 3) {
         const x = outputs[i];
         const y = outputs[i + 1];
+        maxX = Math.max(x, maxX);
+        maxY = Math.max(y, maxY);
+
         const type = outputs[i + 2];
 
         screen.set(`${x}_${y}`, type);
     }
     const numBlocks = [...screen.values()].filter(value => value === 2).length;
     console.log('blocks', numBlocks);
+
+    console.log('x', maxX, 'y', maxY);
 };
+
+// Max X = 36
+// Max Y = 19
+// Make the screen uh, 37x20? 40x20? 40x40?
 
 go();
