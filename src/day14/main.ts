783351bfd2517aebc3b66f0f0bb97b5d076e6578
Isaac Simmons <isaac.simmons@gmail.com>
2019-12-14 10:32:39 -0500

    Part2, just do it in a shitty binary search

--- src/day14/main.ts
+++ src/day14/main.ts
@@ -26,83 +26,107 @@ const parseReaction = (s : string): Reaction => {
 };
 
 const reactions = inputText.trim().split('\n').map(parseReaction);
-console.log(reactions);
-
-// TODO: dynaic programming?
+// console.log(reactions);
 
 const recipes = new Map<string, Reaction>(reactions.map(reaction => [reaction.output.name, reaction]));
 
-const requirements = new Map<string, number>([['FUEL', 1]]);
-let oreNeeded = 0;
-const surplus = new Map<string, number>();
+const oreForFuel = (fuel: number) => {
+    const requirements = new Map<string, number>([['FUEL', fuel]]);
+    let oreNeeded = 0;
+    const surplus = new Map<string, number>();
+    
+    const ORE = 'ORE';
+    
+    const withdrawSurplus = (chemical: string, amount: number) => {
+        if (chemical === ORE) {
+            return 0;
+        }
+        const amountFromSurplus = surplus.get(chemical);
+        if (!amountFromSurplus) {
+            return 0;
+        }
+    
+        if (amountFromSurplus > amount) {
+            surplus.set(chemical, amountFromSurplus - amount);
+            return amount;
+        } else {
+            surplus.delete(chemical);
+            return amountFromSurplus;
+        }
+    };
+    
+    const addSurplus = (chemical: string, amount: number) => {
+        if (amount > 0) {
+            surplus.set(chemical, amount + (surplus.get(chemical) || 0));
+        }
+    };
+    
+    const addRequirements = (chemical: string, amount: number) => {
+        if (chemical === ORE) {
+            oreNeeded += amount;
+        } else {
+            // TODO: are surplus and requirements the same Map, just with negative values allowed?
+            requirements.set(chemical, amount + (requirements.get(chemical) || 0));
+        }
+    };
+    
+    do {
+        // console.log('ore', oreNeeded);
+        // console.log('requirements', requirements);
+        // console.log('surplus', surplus);
+    
+        const chemical = requirements.keys().next().value;
+        const amountNeeded = requirements.get(chemical)!;
+        // console.log('Making', amountNeeded, 'of', chemical);
+        requirements.delete(chemical);
+    
+        const amountFromSurplus = withdrawSurplus(chemical, amountNeeded);
+        const amountToSynthesize = amountNeeded - amountFromSurplus;
+    
+        if (amountToSynthesize <= 0) {
+            continue;
+        }
+    
+        const ingredients = recipes.get(chemical);
+        if (!ingredients) {
+            throw new Error('No formula to make ' + chemical);
+        }
+    
+        const recipeTimes = Math.ceil(amountToSynthesize / ingredients.output.quantity);
+    
+        // Any extra that we made gets stored in surplus
+        addSurplus(chemical, recipeTimes * ingredients.output.quantity - amountToSynthesize);
+        ingredients.inputs.forEach(input => {
+            const inputAmountNeeded = input.quantity * recipeTimes;
+            const inputFromSurplus = withdrawSurplus(input.name, inputAmountNeeded);
+            addRequirements(input.name, inputAmountNeeded - inputFromSurplus);
+        });
+    
+    } while (requirements.size > 0);
+    
+    return oreNeeded;
+}
 
-const ORE = 'ORE';
+// Find the highest fuel amount that can be made with <= 1T ore
 
-const withdrawSurplus = (chemical: string, amount: number) => {
-    if (chemical === ORE) {
-        return 0;
-    }
-    const amountFromSurplus = surplus.get(chemical);
-    if (!amountFromSurplus) {
-        return 0;
-    }
+const oreForOneFuel = oreForFuel(1);
+const ORE_MAX = 1_000_000_000_000;
 
-    if (amountFromSurplus > amount) {
-        surplus.set(chemical, amountFromSurplus - amount);
-        return amount;
-    } else {
-        surplus.delete(chemical);
-        return amountFromSurplus;
-    }
-};
-
-const addSurplus = (chemical: string, amount: number) => {
-    if (amount > 0) {
-        surplus.set(chemical, amount + (surplus.get(chemical) || 0));
-    }
-};
-
-const addRequirements = (chemical: string, amount: number) => {
-    if (chemical === ORE) {
-        oreNeeded += amount;
-    } else {
-        // TODO: are surplus and requirements the same Map, just with negative values allowed?
-        requirements.set(chemical, amount + (requirements.get(chemical) || 0));
-    }
-};
+let lowerBound = Math.floor(ORE_MAX / oreForOneFuel); // I know I can make this much fuel
+let upperBound: number|undefined = undefined; // I know I can't make this much fuel
 
 do {
-    console.log('ore', oreNeeded);
-    console.log('requirements', requirements);
-    console.log('surplus', surplus);
-
-    const chemical = requirements.keys().next().value;
-    const amountNeeded = requirements.get(chemical)!;
-    console.log('Making', amountNeeded, 'of', chemical);
-    requirements.delete(chemical);
-
-    const amountFromSurplus = withdrawSurplus(chemical, amountNeeded);
-    const amountToSynthesize = amountNeeded - amountFromSurplus;
-
-    if (amountToSynthesize <= 0) {
-        continue;
+    const valueToTest: number = upperBound === undefined
+      ? (lowerBound * 2)
+      : Math.floor((upperBound + lowerBound) / 2);
+    // console.log('between', lowerBound, 'and', upperBound);
+    // console.log('can I make', valueToTest, 'fuel?');
+    const oreForTest = oreForFuel(valueToTest);
+    if (oreForTest > ORE_MAX) {
+        upperBound = valueToTest;
+    } else {
+        lowerBound = valueToTest;
     }
+} while (upperBound === undefined || upperBound - lowerBound > 1);
 
-    const ingredients = recipes.get(chemical);
-    if (!ingredients) {
-        throw new Error('No formula to make ' + chemical);
-    }
-
-    const recipeTimes = Math.ceil(amountToSynthesize / ingredients.output.quantity);
-
-    // Any extra that we made gets stored in surplus
-    addSurplus(chemical, recipeTimes * ingredients.output.quantity - amountToSynthesize);
-    ingredients.inputs.forEach(input => {
-        const inputAmountNeeded = input.quantity * recipeTimes;
-        const inputFromSurplus = withdrawSurplus(input.name, inputAmountNeeded);
-        addRequirements(input.name, inputAmountNeeded - inputFromSurplus);
-    });
-
-} while (requirements.size > 0);
-
-console.log(oreNeeded);
+console.log('most fuel I can make is', lowerBound);
